ffa502c455a563bff40915f253d591cf66ab62dc
kurasmagnolia <kingmmcleod@outlook.com>
2025-03-07 23:55:20 -0500

    adding api keys to variables

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,22 @@
-/* 
-GifSearch is a controlled form that sets a search term to find gifs
-GifContainer must take the search term and then fetch gifs according from the search/ endpoint
+/**
+ * GifSearch is a controlled form that sets a search term to find gifs
+ * GifContainer must take the search term and then fetch gifs according from the search/ endpoint
+ *
+ * TODO:
+ * - Share the searchTerm state set by the GifSearch form with the GifContainer
+ *
+ * @format
+ */
 
-TODO:
-- Share the searchTerm state set by the GifSearch form with the GifContainer
-*/
+import NavBar from './components/NavBar';
+import GifContainer from './components/GifContainer';
+import GifSearch from './components/GifSearch';
 
-import NavBar from './components/NavBar'
-import GifContainer from './components/GifContainer'
-import GifSearch from './components/GifSearch'
+const trendAPI =
+  'https://api.giphy.com/v1/gifs/trending?api_key=API_KEY&rating=g';
+
+const searchAPI =
+  'https://api.giphy.com/v1/gifs/search?api_key=API_KEY&q=QUERY&rating=g';
 
 const App = () => {
   return (
@@ -21,6 +29,6 @@ const App = () => {
       </div>
     </div>
   );
-}
+};
 
 export default App;
